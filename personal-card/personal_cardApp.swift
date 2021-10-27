13018518c83351fefab833a8c12bf835bfb6809b
VIctor <colen.victor@icloud.com>
2021-10-27 15:33:30 -0300

    Initial Commit

---
+++ personal-card/personal_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_cardApp.swift
+//  personal-card
+//
+//  Created by Victor Colen on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct personal_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
